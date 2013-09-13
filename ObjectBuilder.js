5881dc338758e0dae55b0a269ccecc74f305f481
Danilo de Castro <daniloster@gmail.com>
2013-09-13 08:39:15 -0300

    Update ObjectBuilder.js

--- ObjectBuilder.js
+++ ObjectBuilder.js
@@ -1,13 +1,4 @@
-var A = function(firstName, lastName, idade) {
-    Object.property(this, "firstName", "string", firstName, true);
-    Object.property(this, "lastName", "string", lastName, true);
-    Object.property(this, "idade", "number", idade, false);
-    Object.property(this, "fullName", function(prefix) {
-        return (prefix ? prefix + " ": "") + this.firstName() + " " + this.lastName();
-    });
-};
-
-Object.property = function(instance, name, typeOrFunction, value, isReadOnly) {
+Object.property = function(instance, name, type, value, isReadOnly) {
     types = {
         number: function(v) { 
             return v instanceof Number || typeof v === typeof 1;
@@ -25,38 +16,25 @@ Object.property = function(instance, name, typeOrFunction, value, isReadOnly) {
             return v instanceof Object || v instanceof {} || typeof v === typeof {};
         }
     };
-    if (typeof typeOrFunction === typeof function(){}) {
-        instance[name] = typeOrFunction;
-    } else {
-        instance[name] = function(val) {
-            if (val) {
-                var typeInfo = typeof val;
-                if (isReadOnly) {
-                    throw new Error("The property " + name + " is read-only.");
-                } else {
-                    if (types[typeOrFunction](val)) {
-                        value = val;
-                        return instance;
-                    } else {
-                        throw new Error("Type is wrong. Found '" + typeInfo + "' while expected '" + typeOrFunction + "'");
-                    }
-                }
+    instance[name] = function(val) {
+        if (val) {
+            var typeInfo = typeof val;
+            if (isReadOnly) {
+                throw new Error("The property " + name + " is read-only.");
             } else {
-                return value;
+                if (types[type](val)) {
+                    value = val;
+                    return instance;
+                } else {
+                    throw new Error("Type is wrong. Found '" + typeInfo + "' while expected '" + type + "'");
+                }
             }
-        };
-    }
+        } else {
+            return value;
+        }
+    };
 };
 
-try { 
-    var a = new A("Danilo", "Castro", 29);
-    console.log(a);
-    document.getElementById("sample-1").innerHTML = a.idade();
-    document.getElementById("sample-2").innerHTML = a.idade(22).idade();
-    document.getElementById("sample-3").innerHTML = a.firstName();
-    document.getElementById("sample-31").innerHTML = a.fullName();
-    document.getElementById("sample-32").innerHTML = a.fullName("Sr. ");
-    a.idade("asd asdasda");
-} catch (e) {
-    $("#sample-4").html("ERROR: " + e);
-}
+Object.method = function(instance, name, calling) {
+    instance[name] = calling;
+};
